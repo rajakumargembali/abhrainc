68a595fa7f4fe00d4c86a16b34408641b0c23601
Sai Praneeth <praneeth.anumula@abhrainc.com>
2016-12-29 15:48:43 +0530

    adding updated code

---
+++ abhrainccockpits/gensrc/com/abhrainc/cockpits/constants/GeneratedAbhraincCockpitsConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Dec 28, 2016 7:08:34 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.abhrainc.cockpits.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedAbhraincCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "abhrainccockpits";
+	
+	protected GeneratedAbhraincCockpitsConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ abhraincfacades/gensrc/com/abhrainc/facades/constants/GeneratedAbhraincFacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Dec 28, 2016 7:08:34 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.abhrainc.facades.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedAbhraincFacadesConstants
+{
+	public static final String EXTENSIONNAME = "abhraincfacades";
+	
+	protected GeneratedAbhraincFacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
