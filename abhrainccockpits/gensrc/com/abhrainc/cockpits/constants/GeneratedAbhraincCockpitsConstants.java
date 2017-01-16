18ab5b2638af0045cac059b721603d0c6e659bc0
Sujan7 <sujan.shanthi@abhrainc.com>
2017-01-17 03:02:40 +0530

    updated

--- abhrainccockpits/gensrc/com/abhrainc/cockpits/constants/GeneratedAbhraincCockpitsConstants.java
+++ abhrainccockpits/gensrc/com/abhrainc/cockpits/constants/GeneratedAbhraincCockpitsConstants.java
@@ -1,12 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at Jan 4, 2017 3:08:38 PM                      ---
- * --- Generated at 3 Jan, 2017 4:37:52 PM                      ---
- * --- Generated at 6 Jan, 2017 5:29:56 PM                      ---
- * --- Generated at 16 Jan, 2017 5:45:40 PM                     ---
- * --- Generated at Jan 16, 2017 5:32:46 PM                     ---
- * --- Generated at 15 Jan, 2017 6:49:53 PM                     ---
+ * --- Generated at 17 Jan, 2017 2:55:52 AM                     ---
  * ----------------------------------------------------------------
  */
 package com.abhrainc.cockpits.constants;
--- abhrainctest/gensrc/com/abhrainc/test/constants/GeneratedAbhraincTestConstants.java
+++ abhrainctest/gensrc/com/abhrainc/test/constants/GeneratedAbhraincTestConstants.java
@@ -1,6 +1,8 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 17 Jan, 2017 2:55:52 AM                     ---
+ * ----------------------------------------------------------------
  */
 package com.abhrainc.test.constants;
 
@@ -8,16 +10,15 @@ package com.abhrainc.test.constants;
  * @deprecated use constants in Model classes instead
  */
 @Deprecated
-@SuppressWarnings(
-{ "unused", "cast", "PMD" })
+@SuppressWarnings({"unused","cast","PMD"})
 public class GeneratedAbhraincTestConstants
 {
 	public static final String EXTENSIONNAME = "abhrainctest";
-
+	
 	protected GeneratedAbhraincTestConstants()
 	{
 		// private constructor
 	}
-
-
+	
+	
 }
