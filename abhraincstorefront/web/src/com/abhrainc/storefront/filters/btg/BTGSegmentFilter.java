f852aaaf088e682ed39060c9e6816a10a2d4facf
Sujan7 <sujan.shanthi@abhrainc.com>
2017-02-07 15:18:36 +0530

    updated

--- abhraincstorefront/web/src/com/abhrainc/storefront/filters/btg/BTGSegmentFilter.java
+++ abhraincstorefront/web/src/com/abhrainc/storefront/filters/btg/BTGSegmentFilter.java
@@ -9,12 +9,11 @@
  * Information and shall use it only in accordance with the terms of the
  * license agreement you entered into with hybris.
  *
- *  
+ *
  */
 package com.abhrainc.storefront.filters.btg;
 
 import de.hybris.platform.cms2.misc.CMSFilter;
-import com.abhrainc.storefront.filters.btg.support.BTGSegmentStrategy;
 
 import java.io.IOException;
 
@@ -23,20 +22,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.abhrainc.storefront.filters.btg.support.BTGSegmentStrategy;
 
 
 /**
  * Filter that evaluates the BTG context for the current request. This is a spring configured filter that is executed by
- * the PlatformFilterChain.
- * The segments are evaluated after the nested chain is executed.
+ * the PlatformFilterChain. The segments are evaluated after the nested chain is executed.
  */
 public class BTGSegmentFilter extends OncePerRequestFilter implements CMSFilter
 {
-	@SuppressWarnings("unused")
-	private static final Logger LOG = Logger.getLogger(BTGSegmentFilter.class);
 
 	private BTGSegmentStrategy btgSegmentStrategy;
 
